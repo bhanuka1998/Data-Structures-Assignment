19c954f685ce9b5cb26667d8b8cbce128804b76e
CB007874 <cb007874@students.apiit.lk>
2019-10-19 05:40:49 +0530

    implemented delete customer method

--- customer.py
+++ customer.py
@@ -1,18 +1,18 @@
 class Customer:
-    def __init__(self, name, address, vehicle_no, contact_no, nic_no, customer_type="General"):
+    def __init__(self, name, address, vehicle_no, contact_no, nic_no, customer_id, customer_type="General"):
         self.name = name
         self.address = address
         self.vehicle_no = vehicle_no
         self.contact_no = contact_no
         self.nic_no = nic_no
         self.customer_type = customer_type
-        print(name, address, vehicle_no, contact_no, nic_no, customer_type)
+        self.customer_id = customer_id
+
 
 class Clean_park:
-    def __init__(self, customer1):
-        self.customer1 = customer_list = []
-        customer_list.append(customer1)
-        print(customer_list)
+    def __init__(self):
+        self.customer_list = []
+        self.customer_id_list = []
 
     def add_customer(self):
         name = input("Input Customer Name: ")
@@ -21,9 +21,20 @@ class Clean_park:
         contact_no = input("Input Contact Number: ")
         nic_no = input("Input NIC Number: ")
         # return name, address, vehicle_no, contact_no, nic_no
-        self.customer1 = Customer(name, address, vehicle_no, contact_no, nic_no)
-       # customer_list = [customer1]
-       # print(customer_list)
+        if len(self.customer_list) is 0:
+            customer_id = 0
+        else:
+            customer_id = self.customer_list[-1].customer_id + 1
+        self.customer_list.append(Customer(name, address, vehicle_no, contact_no, nic_no, customer_id))
+        self.customer_id_list.append(customer_id)
+
+    def remove_customer(self):
+        to_delete = int(input("Input Customer ID: "))
+        if to_delete in self.customer_id_list:
+            self.customer_list = [customer for customer in self.customer_list if customer.customer_id != to_delete]
+            self.customer_id_list.remove(to_delete)
+        else:
+            print("No customer under this identity")
 
     def select_option(self):
         while True:
@@ -40,8 +51,16 @@ class Clean_park:
             print("Enter No.8 to view the service status")
             choice = int(input("Select option: "))
             if choice is 1:
-                Clean_park.add_customer()
+                self.add_customer()
+            elif choice is 2:
+                self.remove_customer()
+            elif choice is 3:
+                pass
+            elif choice is 4:
+                for customer in self.customer_list:
+                    print("Customer Name: ", customer.name, "Customer ID: ", customer.customer_id)
+
 
 
 menu1 = Clean_park()
-cus1 = Customer(menu1.select_option())
+menu1.select_option()
